1b9c9892fd1afacf8636eb7b5b86e3f2adb72d12
motaher77899 <bijoy77899@gmail.com>
2025-11-02 13:19:51 +0300

    2nd

---
+++ lib/models/admin.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class MatchModel {
+  final String id;
+
+
+  MatchModel({
+    required this.id,
+
+  });
+
+
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+
+    };
+  }
+
+  // Match time এর জন্য helper
+
